f4d7a7d81fadc836156057ac87b86680b7675cd8
Hannah <hannah.v.meyer@gmail.com>
2018-09-10 06:48:41 +0000

    Add function for estimating effective number of tests

--- GWAS/utils.r
+++ GWAS/utils.r
@@ -20,3 +20,24 @@ pseudoMultitrait <- function(summarystats) {
     multitrait_p <- as.vector(pchisq(chi2stats, df, lower.tail=FALSE))
     return(multitrait_p)
 }
+
+#' Estimate effective number of tests
+#'
+#' Use eigenvalues of phenotype correlation matrix to estimate the effective
+#' number of tests conducted. Result can be taken to adjust for Bonferoni-type
+#' multiple hypothesis, with p/m_effective rather than p/m_total.
+#' @param mat [N x P] matrix of phenotypes [double] from N samples and P tested
+#' traits.
+#' return Effective number of tests [double]
+#' @references Galwey (2009) A new measure of the effective number of tests,
+#' a practical tool for comparing families of non-independent significance
+#' tests. Genetic Epidemiology.
+Teff <- function(mat) {
+    # 1. get correlation matrix
+    corr_matrix <-  cor(mat,  method="spearman")
+    # 2. Get eigenvalues of correlation matrix:
+    eigenval <- eigen(corr_matrix, only.value=TRUE, symmetric=TRUE)$values
+    # 3. Determine effective number of tests:
+    t <- sum(sqrt(eigenval))^2/sum(eigenval)
+    return(t)
+}
