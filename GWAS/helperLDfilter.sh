c1cc617a6e5d32e2071de68d2a8a9dc1ad0d8836
Hannah <hannah.v.meyer@gmail.com>
2018-10-10 15:30:53 +0000

    Add header to output file

--- GWAS/helperLDfilter.sh
+++ GWAS/helperLDfilter.sh
@@ -2,4 +2,5 @@ sig=$1
 tags=$2
 out=$3
 
-awk 'NR==FNR {snp[$1]=$0; next} $1 in snp {print $0}' $sig $tags > $out
+head -n 1 $tags > $out
+awk 'NR==FNR {snp[$1]=$0; next} $1 in snp {print $0}' $sig $tags >> $out
